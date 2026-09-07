56877a717b7fbcd4d2784ebe0ec5e930e34b2eb0
trevisaq <souzagui2t>
2026-09-06 23:00:32 -0300

    E6 - Personagens de RPG

--- index.php
+++ index.php
@@ -201,24 +201,79 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 
-//  ================================= EXERCICIO 5 (TERMOSTATO INTELIGENTE) =================================
+// //  ================================= EXERCICIO 5 (TERMOSTATO INTELIGENTE) =================================
 
-use App\TermostatoInteligente;
+// use App\TermostatoInteligente;
 
-$T1 = new TermostatoInteligente(25.0, 17.0);
-$T2 = new TermostatoInteligente(12.0, 22.0);
+// $T1 = new TermostatoInteligente(25.0, 17.0);
+// $T2 = new TermostatoInteligente(12.0, 22.0);
 
-echo "\n====================== TERMOSTADO 1 ======================\n\n";
-echo "Ação necessária: " . $T1->acaoNecessaria() . PHP_EOL; // Verifica que está desligado
-$T1->ligar(); // Envio a ação para ligar
-echo "Ação necessária: " . $T1->acaoNecessaria() . PHP_EOL; // Le a temperatura alvo e define a ação necessária.
+// echo "\n====================== TERMOSTADO 1 ======================\n\n";
+// echo "Ação necessária: " . $T1->acaoNecessaria() . PHP_EOL; // Verifica que está desligado
+// $T1->ligar(); // Envio a ação para ligar
+// echo "Ação necessária: " . $T1->acaoNecessaria() . PHP_EOL; // Le a temperatura alvo e define a ação necessária.
 
-echo "\n====================== TERMOSTADO 2 ======================\n\n";
+// echo "\n====================== TERMOSTADO 2 ======================\n\n";
 
-$T2->ligar();
-echo "Ação necessária: " . $T2->acaoNecessaria() . PHP_EOL; // Le a temperatura alvo e define a ação necessária.
-$T2->atualizarTemperaturaAtual(22); // Atualiza a temperatura atual do termostato 2
-echo "Ação necessária: " . $T2->acaoNecessaria() . PHP_EOL; // Percebe a nova temperatura e define a nova ação (se manter)
+// $T2->ligar();
+// echo "Ação necessária: " . $T2->acaoNecessaria() . PHP_EOL; // Le a temperatura alvo e define a ação necessária.
+// $T2->atualizarTemperaturaAtual(22); // Atualiza a temperatura atual do termostato 2
+// echo "Ação necessária: " . $T2->acaoNecessaria() . PHP_EOL; // Percebe a nova temperatura e define a nova ação (se manter)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  ====================================== EXERCICIO 6 (PERSONAGEM DE RPG) ======================================
+
+use App\PersonagemRPG;
+
+$P1 = new PersonagemRPG("Eleven", 150, 150, 300);
+$P2 = new PersonagemRPG("Demogorgon", 300, 300, 250);
+
+
+echo $P1->status();
+echo"\n========================= FICHAS =========================\n";
+echo $P2->status();
+
+
+$P1->sofrerDano($P2->executarAtaque(80, 70));   
+echo"\n\n";
+echo "1. Demogorgon ataca eleven!!, (Vida de eleven cai de 150 para 80. energia de demogorgon cai de 250 para 170)";
+echo"\n";
+$P1->curar(30); // Vida de Eleven sobe de 80 para 110
+echo"\n";
+echo "2. Eleven decide se curar para recuperar o hp perdido!!, (Vida de eleven sobe 30 pontos)";
+echo"\n";
+$P2->descansar(); // Demogorgon decide descansar para recuperar pontos de energia (170 -> 200)
+echo"\n";
+echo "3. Demogordon decide se curar!! (demogorgon recupera 30 pontos de energia)";
+echo"\n";
+$P2->sofrerDano($P1->executarAtaque(280, 500));
+echo"\n";
+echo "4. Eleven utiliza toda sua energia para matar Demogorgon com um ataque surpresa!!! (A vida de demogorgon e a energia de Eleven zera)";
+echo"\n\n";
+
+
+echo $P1->status();
+echo"\n========================= FICHAS PÓS BATALHA =========================\n";
+echo $P2->status();
+
+
+// echo"\n========================= TESTES PÓS BATALHA =========================\n";
+
+// $P1->executarAtaque(280, 500); // Erro ao tentar realizar ataque sem energia
+// $P1->curar(500); // Erro ao tentar curar a vida acima da vida máxima
+// echo $P1->status();
 
 ?>
---
+++ src/PersonagemRPG.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App;
+use InvalidArgumentException;
+
+class PersonagemRPG
+{
+    // Função inicializadore, exeuctada apenas qunado um objeto é criado
+    public function __construct(
+        private string $nome,
+        private int $vidaMaxima,
+        private int $vidaAtual,
+        private int $energia
+    ) {
+        if (trim($this->nome) === ''){
+            throw new InvalidArgumentException("É necessário informar o nome do personagem");
+        }
+        if ($this->vidaMaxima <= 0){
+            throw new InvalidArgumentException("É necessário que a vida máxima do personagem seja maior que 0");
+        }
+    }
+    public function sofrerDano(int $dano): void
+    {
+        if($dano > 0){
+            if ($dano >= $this->vidaAtual){
+                // Se o dano execer a vida atual, simplesmente defino que a vida retorna como 0
+                $this->vidaAtual = 0;
+            } else{
+                $this->vidaAtual -= $dano;
+            }
+        } else{
+            throw new InvalidArgumentException("Informe um valor de dano válido! (Sendo ele positivo)");
+        }
+    }
+
+    public function curar(int $pontos): void
+    {
+        if($pontos > 0){
+            if ($this->vidaAtual + $pontos >= $this->vidaMaxima){
+                throw new InvalidArgumentException("Informe um valor de pontos de cura válido! (Não podendo execeder o limite da vida máxima)");
+            } else{
+                $this->vidaAtual += $pontos;
+            }
+        } else{
+            throw new InvalidArgumentException("Informe um valor de pontos de cura válido! (Sendo ele positivo)");
+        }
+    }
+
+    public function executarAtaque(int $custoEnergia, int $danoBase): int
+    {   
+        if ($this->vidaAtual <= 0) {
+            throw new InvalidArgumentException("Você não pode realizar ataques com um personagem derrotado!");
+        }
+        if ($this->energia < $custoEnergia) {
+            throw new InvalidArgumentException("Você não pode realizar ataques com um personagem sem energia suficiente!");
+        }
+
+        $this->energia -= $custoEnergia;
+        return $danoBase;
+    }   
+    
+    public function descansar(): void 
+    {   
+        $this->energia = min(300, $this->energia + 30);
+    }   
+    
+    public function estaVivo(): bool
+    {
+        if ($this->vidaAtual > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function status(): string
+    {
+        return "Nome: {$this->nome}" . PHP_EOL . "Vida maxima: {$this->vidaMaxima}" . PHP_EOL . "Vida atual: {$this->vidaAtual}" . PHP_EOL . "Energia: {$this->energia}";
+    }
+}
+?>
